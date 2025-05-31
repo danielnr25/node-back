16044c6c431b4082f33dc26d8fe59311e023fedb
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2025-05-30 20:57:22 -0500

    semana07-node

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // request(req): representa la solicitud del cliente
 // response(res): se usa para enviar la respuesta al cliente
 module.exports.login = async(req,res) =>{
@@ -12,10 +13,13 @@ module.exports.login = async(req,res) =>{
             const user = results[0]; // solamente la informacion de los datos del usuario
             const isMatch = await bcrypt.compare(password,user.password) // true o false
             if(isMatch){
-                return res.status(200).json({
-                    message: "Usuario autenticado con éxito.",
-                    data:user
-                })
+                const payload = {
+                    id:user.id,
+                    username:user.username,
+                    role:user.tipo_usuario_id
+                }
+                const token = jwt.sign(payload,process.env.CLAVE_SECRETA,{expiresIn:"24h"});
+                return res.status(200).json({  message: "Usuario autenticado con éxito.",data:user,token})
             }else{
                 return res.status(401).json({
                     message: "Contraseña incorrecta, verifique nuevamente."
